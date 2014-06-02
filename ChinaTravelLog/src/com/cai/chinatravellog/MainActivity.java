28e903bc70beb3a6a8f5ee5a4057728c7f255416
ycai2 <cai.eason@gmail.com>
2014-06-02 23:53:53 +0800

    Buttons
    Set up buttons for logging. Ready for location service to be
    implemented.

--- ChinaTravelLog/src/com/cai/chinatravellog/MainActivity.java
+++ ChinaTravelLog/src/com/cai/chinatravellog/MainActivity.java
@@ -34,14 +34,19 @@ public class MainActivity extends ActionBarActivity {
 		setContentView(R.layout.fragment_main);  
 		mMapView=(MapView)findViewById(R.id.bmapsView);  
 		mMapView.setBuiltInZoomControls(true);  
+		
+		
 		//设置启用内置的缩放控件  
 		MapController mMapController=mMapView.getController();  
-		// 得到mMapView的控制权,可以用它控制和驱动平移和缩放  
+		// 得到mMapView的控制权,可以用它控制和驱动平移和缩放
+		
 		GeoPoint point =new GeoPoint((int)(39.915* 1E6),(int)(116.404* 1E6));  
 		//用给定的经纬度构造一个GeoPoint，单位是微度 (度 * 1E6)  
 		mMapController.setCenter(point);//设置地图中心点  
 		mMapController.setZoom(12);//设置地图zoom级别  
 	}
+	
+	
 	
 	@Override  
 	protected void onDestroy(){  
