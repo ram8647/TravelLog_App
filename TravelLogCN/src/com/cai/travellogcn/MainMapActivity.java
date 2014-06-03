c7d6a00b46bcbe8fd1f1cbb0684150280a4f3abd
ycai2 <cai.eason@gmail.com>
2014-06-04 01:26:59 +0800

    Location Service for Travel Log CN
    Got Location Service working. The app now locates user once opened.

--- TravelLogCN/src/com/cai/travellogcn/MainMapActivity.java
+++ TravelLogCN/src/com/cai/travellogcn/MainMapActivity.java
@@ -1,18 +1,35 @@
 package com.cai.travellogcn;
 
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.location.LocationManagerProxy;
+import com.amap.api.location.LocationProviderProxy;
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MyLocationStyle;
 
 
 
-public class MainMapActivity extends ActionBarActivity {  
+public class MainMapActivity extends ActionBarActivity implements LocationSource,
+AMapLocationListener{  
     private MapView mapView;  
-    private AMap aMap;  
+    private AMap aMap;
+    private UiSettings mUiSettings;
+	private OnLocationChangedListener mListener;
+	private LocationManagerProxy mAMapLocationManager;
+	private Marker marker;
   
     @Override  
     protected void onCreate(Bundle savedInstanceState) {  
@@ -29,9 +46,33 @@ public class MainMapActivity extends ActionBarActivity {
      */  
     private void init() {  
         if (aMap == null) {  
-            aMap = mapView.getMap();  
+            aMap = mapView.getMap();
+            setUpMap();
+            
         }  
-    }  
+        
+    } 
+    
+    /**
+	 * 设置一些amap的属性
+	 */
+	private void setUpMap() {
+		// 自定义系统定位小蓝点
+		MyLocationStyle myLocationStyle = new MyLocationStyle();
+		myLocationStyle.myLocationIcon(BitmapDescriptorFactory
+				.defaultMarker());// 设置小蓝点的图标
+		myLocationStyle.strokeColor(Color.BLACK);// 设置圆形的边框颜色
+		myLocationStyle.radiusFillColor(Color.argb(100, 0, 0, 180));// 设置圆形的填充颜色
+		// myLocationStyle.anchor(int,int)//设置小蓝点的锚点
+		myLocationStyle.strokeWidth(0.1f);// 设置圆形的边框粗细
+		aMap.setMyLocationStyle(myLocationStyle);
+		aMap.setMyLocationRotateAngle(180);
+		aMap.setLocationSource(this);// 设置定位监听
+		aMap.getUiSettings().setScrollGesturesEnabled(false);//disable gesture scroll
+		aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+	}
+
+    
   
     /** 
      * 方法必须重写 
@@ -48,7 +89,8 @@ public class MainMapActivity extends ActionBarActivity {
     @Override  
     protected void onPause() {  
         super.onPause();  
-        mapView.onPause();  
+        mapView.onPause(); 
+        deactivate();
     }  
       
     /** 
@@ -88,6 +130,77 @@ public class MainMapActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 定位成功后回调函数
+	 */
+	@Override
+	public void onLocationChanged(AMapLocation aLocation) {
+		if (mListener != null && aLocation != null) {
+			mListener.onLocationChanged(aLocation);// 显示系统小蓝点
+			marker.setPosition(new LatLng(aLocation.getLatitude(), aLocation
+					.getLongitude()));// 定位雷达小图标
+			float bearing = aMap.getCameraPosition().bearing;
+			aMap.setMyLocationRotateAngle(bearing);// 设置小蓝点旋转角度
+		}
+	}
+
+	/**
+	 * 激活定位
+	 */
+	@Override
+	public void activate(OnLocationChangedListener listener) {
+		mListener = listener;
+		if (mAMapLocationManager == null) {
+			mAMapLocationManager = LocationManagerProxy.getInstance(this);
+			/*
+			 * mAMapLocManager.setGpsEnable(false);
+			 * 1.0.2版本新增方法，设置true表示混合定位中包含gps定位，false表示纯网络定位，默认是true Location
+			 * API定位采用GPS和网络混合定位方式
+			 * ，第一个参数是定位provider，第二个参数时间最短是2000毫秒，第三个参数距离间隔单位是米，第四个参数是定位监听者
+			 */
+			mAMapLocationManager.requestLocationUpdates(
+					LocationProviderProxy.AMapNetwork, 2000, 10, this);
+		}
+	}
+
+	/**
+	 * 停止定位
+	 */
+	@Override
+	public void deactivate() {
+		mListener = null;
+		if (mAMapLocationManager != null) {
+			mAMapLocationManager.removeUpdates(this);
+			mAMapLocationManager.destroy();
+		}
+		mAMapLocationManager = null;
+	}
+
 
 
 
